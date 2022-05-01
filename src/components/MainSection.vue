1884e17bc0713c53ccc7679b95f07d6ef27364b1
Lukasz Fuchs <lukaszfuchs18@gmail.com>
2022-05-02 01:42:22 +0200

    add buttonh

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -5,7 +5,18 @@
         <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
           <SplideSlide>
             <i-card class="box">
-             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" />
+              <img
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+              />
+              <template #footer>
+                Card Footer
+              </template>
+            </i-card>
+          </SplideSlide>
+
+          <SplideSlide>
+            <i-card class="box">
+              Czesc
               <template #footer>
                 Card Footer
               </template>
@@ -16,13 +27,12 @@
             <i-card class="box">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
+
               <template #footer>
-                Card Footer
+              <i-button color="light">Żłap go</i-button>
               </template>
             </i-card>
           </SplideSlide>
-
-          `
         </Splide>
       </i-column>
     </i-row>
@@ -35,15 +45,13 @@ import '@splidejs/vue-splide/css/skyblue'
 import '@splidejs/vue-splide/css/sea-green'
 import '@splidejs/vue-splide/css/core'
 
-import {onMounted} from "vue"
- 
-const axios = require("axios")
+import { onMounted } from 'vue'
+
+const axios = require('axios')
 onMounted(async () => {
-  await axios
-    .get('https://pokeapi.co/api/v2/pokemon/')
-    .then(response => {
-      console.log(response.data.results)
-    })
+  await axios.get('https://pokeapi.co/api/v2/pokemon/').then((response) => {
+    console.log(response.data.results)
+  })
 })
 </script>
 
